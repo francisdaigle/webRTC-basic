c5469dee33934dcaa2a6b5650a7ceab3b8f2c30e
Francis Daigle <francis@francisdaigle.com>
2014-01-12 21:05:05 -0700

    Initialized repo

---
+++ script.js
@@ -0,0 +1,34 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+var constraints = {audio: true, video: true};
+var video       = document.querySelector("video");
+
+function successCallback(stream) {
+    
+    window.stream       = stream; // stream available to console
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    
+    var audioContext = new AudioContext();
+    var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+  
+    if (window.URL) {
+        video.src = window.URL.createObjectURL(stream);
+    } else {
+        video.src = stream;
+    }
+    
+    mediaStreamSource.connect(audioContext.destination);
+  
+    video.play();
+}
+
+function errorCallback(error){
+    console.log("navigator.getUserMedia error: ", error);
+}
+
+navigator.getUserMedia(constraints, successCallback, errorCallback);
