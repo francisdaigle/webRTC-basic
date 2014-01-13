fb5af29c98bb8a77b686a9cf3b837acb0e7017ba
Francis Daigle <francis@francisdaigle.com>
2014-01-12 21:15:56 -0700

    Formatted comments.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * script.js
  */
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
